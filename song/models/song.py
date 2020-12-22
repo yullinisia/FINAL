bdeff8bd9231a1870ad2c31e6ae8da47a0f24064
ylnsia <yullinisia0@gmail.com>
2020-12-22 14:41:51 +0700

    fixed some error

--- song/models/song.py
+++ song/models/song.py
@@ -1,12 +1,9 @@
 from django.db import models
-from song.models.album import Album
-from song.models.singer import Singer
-
 
 class Song(models.Model):
     title = models.CharField(max_length=200)
-    singer = models.ForeignKey(Singer, on_delete=models.SET_NULL, null=True)
-    album = models.ManyToManyField(Album)
+    singer = models.ForeignKey('Singer', on_delete=models.SET_NULL, null=True)
+    album = models.ManyToManyField('Album')
     date_published = models.DateField(null=True, blank=True)
 
 class Meta:
