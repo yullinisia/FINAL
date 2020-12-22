b4a933b0b9372a5fb6f35b81e566c190f3013c89
ylnsia <yullinisia0@gmail.com>
2020-12-22 16:23:16 +0700

    fixed some files

--- song/models/song.py
+++ song/models/song.py
@@ -1,5 +1,4 @@
 from django.db import models
-<<<<<<< HEAD
 from song.models.singer import Singer
 from song.models.album import Album
 
@@ -7,13 +6,6 @@ class Song(models.Model):
     title = models.CharField(max_length=200)
     singer = models.ForeignKey(Singer, on_delete=models.SET_NULL, null=True)
     album = models.ManyToManyField(Album)
-=======
-
-class Song(models.Model):
-    title = models.CharField(max_length=200)
-    singer = models.ForeignKey('Singer', on_delete=models.SET_NULL, null=True)
-    album = models.ManyToManyField('Album')
->>>>>>> bdeff8bd9231a1870ad2c31e6ae8da47a0f24064
     date_published = models.DateField(null=True, blank=True)
 
 class Meta:
