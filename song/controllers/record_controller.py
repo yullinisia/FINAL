1dd5b3bde6315d4ed53e33b933d4f813855c1ca9
nadyasafirar <58328340+nadyasafirar@users.noreply.github.com>
2020-12-25 14:57:07 +0700

    i added total visit to the recorcd.html

--- song/controllers/record_controller.py
+++ song/controllers/record_controller.py
@@ -3,13 +3,18 @@ from song.models import Song, Singer, Album
 
 
 def record(request):
-    # get all info here including singers, songs, and albums
+    # we check the session with key ‘num_visits’, if doesn’t exist we set to 1
+    num_visits = request.session.get('num_visits', 1)
+    request.session['num_visits'] = num_visits + 1  # add everytime we reload
+    # get all info here including authors, books, and genres
     num_songs = Song.objects.all().count()
     num_singers = Singer.objects.all().count()
     num_albums = Album.objects.all().count()
+    # pass num_visits as the context to display it on the html
     context = {
         'num_songs': num_songs,
         'num_singers': num_singers,
         'num_albums': num_albums,
+        'total_visit': num_visits,
     }
     return render(request, 'record.html', context=context)
